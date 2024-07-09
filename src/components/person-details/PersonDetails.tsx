72d81f74643979f7bf53ae8d6cc15dd7936e44d4
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2024-07-09 17:16:57 +0200

    folders names fixes

--- src/components/person-details/PersonDetails.tsx
+++ src/components/person-details/PersonDetails.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react"
-import PropertyBox from "@/components/PersonDetails/PropertyBox"
+import PropertyBox from "@/components/person-details/PropertyBox"
 import { Person } from "@/types/people"
 
 type PersonDetailsProps = {
